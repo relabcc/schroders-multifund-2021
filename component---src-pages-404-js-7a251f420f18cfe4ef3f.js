f3b6a51f53ad98979fdadca1d0aa7ad35679c358
Peja <peja@relab.cc>
2021-01-22 12:13:01 +0800

    Updates

---
+++ component---src-pages-404-js-7a251f420f18cfe4ef3f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),l=t.n(a);e.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement("h1",null,"NOT FOUND"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7a251f420f18cfe4ef3f.js.map
